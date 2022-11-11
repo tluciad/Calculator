e8d2fcff44c972ec54ef4ff697184c17864bba8e
tluciad <tluciad@gmail.com>
2022-11-11 15:44:10 -0500

    Updates

---
+++ precache-manifest.b7ed8dbd428a465eb8e085cd70c33b5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ca663268a3e52360020",
+    "url": "/Calculator/static/css/main.581cbc60.chunk.css"
+  },
+  {
+    "revision": "2ca663268a3e52360020",
+    "url": "/Calculator/static/js/main.2ca66326.chunk.js"
+  },
+  {
+    "revision": "715272029e2ae5d09911",
+    "url": "/Calculator/static/js/1.71527202.chunk.js"
+  },
+  {
+    "revision": "546deb05c16eeec89d41",
+    "url": "/Calculator/static/js/runtime~main.546deb05.js"
+  },
+  {
+    "revision": "c00d94842ae093a74ce52becdeacb98b",
+    "url": "/Calculator/static/media/freecodecamp-logo.c00d9484.png"
+  },
+  {
+    "revision": "fe56881d9e1a76b54b46652eb7e5ea4d",
+    "url": "/Calculator/index.html"
+  }
+];
